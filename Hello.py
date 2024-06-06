85a96abcfb34b35a075ce051a7021ce7acfe7e27
SelvakumarItaly <praiseselvaresearch100@gmail.com>
2024-06-06 09:31:50 +0200

    initial commit

---
+++ Hello.py
@@ -0,0 +1,39 @@
+print("Hello, World")
+#integer
+a = 3 
+print(a)
+#float
+b=10.5
+print(b)
+#String
+c="Selva"
+print(c)
+
+#addition, subtraction, multiplecation, division
+
+add = a + b
+subtract=b - a
+multi=b * a
+dive=b / a
+
+print("addition is: ", add)
+print("subtraction is: ", subtract)
+print("multiplication is: ", multi)  
+print("division is: ", dive) 
+
+
+print("Selva")
+
+#Create two variables x and y, assign them values, and print their sum, difference, product, and quotient.
+x=20
+y=5
+
+print("sum : ",x+y)
+print("difference:", x-y)
+print("product: ", x*y)
+print("quotient: ", x/y)
+
+
+
+
+
